738c6d9ce5d8aa0fbe8b700c5c01c2c74d4855cb
Alex Lisitsyn <aleks@espressif.com>
2024-09-30 20:19:38 +0800

    fix build docs, extended data types en option v2, mb_event check

--- docs/_static/modbus_docs_versions.js
+++ docs/_static/modbus_docs_versions.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_VERSIONS = {
     DEFAULTS: { has_targets: true,
-                supported_targets: [ "esp32", "esp32s2", "esp32s3", "esp32c2","esp32c3", "esp32c6", "esp32s2", "esp32s3, esp32p4" ]
+                supported_targets: [ "esp32", "esp32s2", "esp32s3", "esp32c2","esp32c3", "esp32c5", "esp32c6", "esp32s2", "esp32s3, esp32p4" ]
               },
     VERSIONS: [
         { name: "latest" },
@@ -15,6 +15,7 @@ var DOCUMENTATION_VERSIONS = {
        { text: "ESP32-C3", value: "esp32c3"},
        { text: "ESP32-C6", value: "esp32c6"},
        { text: "ESP32-H2", value: "esp32h2"},
-       { text: "ESP32-P4", value: "esp32p4"}
+       { text: "ESP32-P4", value: "esp32p4"},
+       { text: "ESP32-C5", value: "esp32c5"}
     ]
 };
