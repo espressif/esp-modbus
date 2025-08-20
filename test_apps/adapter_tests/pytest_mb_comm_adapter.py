ec76895fae3aae5955b2d600abc026ccefc2a689
aleks <aleks@espressif.com>
2025-08-20 12:36:41 +0200

    fix idf compatibility issues under ci

--- test_apps/adapter_tests/pytest_mb_comm_adapter.py
+++ test_apps/adapter_tests/pytest_mb_comm_adapter.py
@@ -4,14 +4,8 @@
 import pytest
 from pytest_embedded import Dut
 
-
-CONFIGS = [
-    pytest.param('serial', marks=[pytest.mark.esp32, pytest.mark.esp32p4]),
-    pytest.param('tcp', marks=[pytest.mark.esp32, pytest.mark.esp32p4]),
-]
-
-@pytest.mark.temp_skip_ci(targets=['esp32p4'], reason='no multi-dev runner')
+@pytest.mark.parametrize('target', ['esp32'], indirect=True)
+@pytest.mark.parametrize('config', ['serial','tcp'], indirect=True)
 @pytest.mark.multi_dut_modbus_generic
-@pytest.mark.parametrize('config', CONFIGS, indirect=True)
 def test_modbus_comm_adapter(dut: Dut) -> None:
     dut.expect_unity_test_output()
