4d64cbd2be3b02cf68d8ad3a51346f6af43b2e80
Octavio Godoy <octavio.godoy@espressif.com>
2026-01-26 17:25:34 +0100

    feat(refactor_ci_test): Disabling some of the build/test on the test_app to improve CI runtime

--- test_apps/physical_tests/pytest_mb_comm_multi_dev.py
+++ test_apps/physical_tests/pytest_mb_comm_multi_dev.py
@@ -16,15 +16,16 @@ def test_modbus_comm_multi_dev_serial(case_tester) -> None:  # type: ignore
             print(f"Test case: {case.name}")
             case_tester.run_multi_dev_case(case=case, reset=True)
 
+## Disabling for the Ci build and test Refactor
 
-@pytest.mark.parametrize("count, config", [(2, "ethernet")], indirect=True)
-@pytest.mark.parametrize("target", ["esp32"], indirect=True)
-@pytest.mark.multi_dut_modbus_tcp
-def test_modbus_comm_multi_dev_tcp(case_tester) -> None:  # type: ignore
-    for case in case_tester.test_menu:
-        if (
-            case.attributes.get("test_env", "multi_dut_modbus_tcp")
-            == "multi_dut_modbus_tcp"
-        ):
-            print(f"Test case: {case.name}")
-            case_tester.run_multi_dev_case(case=case, reset=True)
+# @pytest.mark.parametrize("count, config", [(2, "ethernet")], indirect=True)
+# @pytest.mark.parametrize("target", ["esp32"], indirect=True)
+# @pytest.mark.multi_dut_modbus_tcp
+# def test_modbus_comm_multi_dev_tcp(case_tester) -> None:  # type: ignore
+#     for case in case_tester.test_menu:
+#         if (
+#             case.attributes.get("test_env", "multi_dut_modbus_tcp")
+#             == "multi_dut_modbus_tcp"
+#         ):
+#             print(f"Test case: {case.name}")
+#             case_tester.run_multi_dev_case(case=case, reset=True)
