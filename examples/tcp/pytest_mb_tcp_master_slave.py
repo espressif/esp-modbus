20f6021b34fb218117fe482b16f899b0570784f6
aleks <aleks@espressif.com>
2026-03-12 16:33:57 +0100

    fix: mypy issue in test

--- examples/tcp/pytest_mb_tcp_master_slave.py
+++ examples/tcp/pytest_mb_tcp_master_slave.py
@@ -201,7 +201,7 @@ def test_modbus_tcp_communication(
 @pytest.mark.parametrize("target", ["esp32"], indirect=True)
 @pytest.mark.multi_dut_modbus_generic
 @pytest.mark.parametrize("config", ["dummy_config"])
-def test_modbus_tcp_generic(config) -> None:
+def test_modbus_tcp_generic(config: str) -> None:
     logger.info("The generic tcp example tests are not provided yet.")
 
 
