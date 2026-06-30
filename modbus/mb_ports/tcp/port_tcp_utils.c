c83a7ea39fdffe3a3defadb8a747ab3f7813ae40
Alex Lisitsyn <aleks@espressif.com>
2026-06-30 15:38:33 +0100

    fix: improve event handling path, timings and throughput

--- modbus/mb_ports/tcp/port_tcp_utils.c
+++ modbus/mb_ports/tcp/port_tcp_utils.c
@@ -245,7 +245,7 @@ int port_read_packet(mb_node_info_t *info_ptr)
             info_ptr->recv_err = ERR_BUF;
             temp = mbap_payload_max; // read all remaining data from buffer
         }
-
+        // Sequential frame read with minimal timeout to reduce delays
         ret = port_get_buf(info_ptr, &ptemp_buf[MB_TCP_UID], temp, MB_READ_TICK);
         if (ret < 0) {
             info_ptr->recv_err = ret;
