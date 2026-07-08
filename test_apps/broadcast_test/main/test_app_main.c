624b566c7e6d57a3e35fa2ad9a09426d19b35402
aleks <aleks@espressif.com>
2026-07-08 15:18:10 +0200

    test: master bcast request

---
+++ test_apps/broadcast_test/main/test_app_main.c
@@ -0,0 +1,22 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2026 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include "unity.h"
+#include "test_common.h"
+
+void app_main(void)
+{
+#if !CONFIG_LOG_DEFAULT_LEVEL_DEBUG
+    esp_log_level_set("mbc_tcp.slave", ESP_LOG_DEBUG);
+    esp_log_level_set("mbc_serial.slave", ESP_LOG_DEBUG);
+    esp_log_level_set("mb_object.slave", ESP_LOG_DEBUG);
+#else
+    // Disable VFS logs as they are too verbose
+    esp_log_level_set("vfs_calls", ESP_LOG_NONE);
+#endif
+    printf("Modbus RS485 multi-device test cases/n");
+    unity_run_menu();
+}
--- test_apps/test_common/test_common.c
+++ test_apps/test_common/test_common.c
@@ -7,6 +7,7 @@
 #include "freertos/portmacro.h"
 #include "freertos/queue.h"
 
+#include "esp_modbus_common.h"
 #include "port_adapter.h"
 #include "mb_common.h"
 #include "mbc_slave.h"
@@ -137,6 +138,27 @@ void test_task_add_entry(TaskHandle_t task_handle, void *inst)
     LIST_INSERT_HEAD(&s_task_list, new_entry, entries);
     portEXIT_CRITICAL(&s_list_spinlock);
     xSemaphoreTake(new_entry->task_sema_handle, 1);
+}
+
+static void *test_task_find_object(bool is_master)
+{
+    task_entry_t *it, *pfound = NULL;
+    if (LIST_EMPTY(&s_task_list)) {
+        return NULL;
+    }
+
+    portENTER_CRITICAL(&s_list_spinlock);
+    LIST_FOREACH(it, &s_task_list, entries) {
+        if (it) {
+            mb_controller_common_t *pobj = (mb_controller_common_t *)it->inst_handle;
+            if (pobj && pobj->mb_base->descr.is_master == is_master) {
+                pfound = (void *)pobj;
+                break;
+            }
+        }
+    }
+    portEXIT_CRITICAL(&s_list_spinlock);
+    return pfound;
 }
 
 static task_entry_t *test_task_find_entry(TaskHandle_t task_handle)
@@ -438,8 +460,16 @@ esp_err_t test_common_read_modbus_parameter(void *handle, uint16_t cid, uint16_t
         uint8_t type = 0;
         if (param_descriptor->mb_slave_addr) {
             err = mbc_master_get_parameter(handle, cid, (uint8_t *)par_data, &type);
-        } else { // if the address is broadcast, try to read from first slave intentionally
-            err = mbc_master_get_parameter_with(handle, cid, param_descriptor->mb_slave_addr + 1, (uint8_t *)par_data, &type);
+        } else {
+            // if the address is broadcast, try to read from first registered slave intentionally
+            void *pobj = test_task_find_object(false);
+            uint8_t uid = 0x01;
+            if (pobj) {
+                mbs_controller_iface_t *pctrl_obj = ((mbs_controller_iface_t *)pobj);
+                uid = pctrl_obj->opts.comm_opts.common_opts.uid;
+            }
+            ESP_LOGD(TAG, "Use slave object: %p, UID:%u", pobj, uid);
+            err = mbc_master_get_parameter_with(handle, cid, uid, (uint8_t *)par_data, &type);
         }
         if (err == ESP_OK) {
             ESP_LOGI(TAG, "%p, CHAR #%u %s (%s) value = (0x%04x) parameter read successful.",
